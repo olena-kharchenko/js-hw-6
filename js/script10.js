4e314a115ebc657599338a23e6feac8a7d01471e
olena-kharchenko <harchenko.es1990@gmail.com>
2020-08-28 14:31:01 +0300

    changes

--- js/script10.js
+++ js/script10.js
@@ -5,15 +5,10 @@ console.table(users);
 // повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
 const getSortedUniqueSkills = array => {
-  const arrayOfSkills = [...array].reduce(
-    (acc, { skills }) => acc.concat(skills),
-    [],
-  );
-
-  return arrayOfSkills
-    .filter((element, index) => arrayOfSkills.indexOf(element) === index)
+  array
+    .reduce((acc, { skills }) => [...acc, ...skills], [])
+    .filter((value, index, array) => array.indexOf(value) === index)
     .sort();
-};
 
 console.log(getSortedUniqueSkills(users));
 // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
